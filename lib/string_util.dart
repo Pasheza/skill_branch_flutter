f8b50046df70d7cc8eaa02f12cc224bca16ae22d
Захаренков Павел <zakharenkovpavel@Zaharenkovs-MacBook-Air.local>
2020-08-15 15:58:01 +0300

    deletion of previous homework

--- lib/string_util.dart
+++
@@ -1,9 +0,0 @@
-mixin UserUtils {
-  String capitalize(String str) =>
-      str[0].toUpperCase() + str.substring(1).toLowerCase();
-}
-
-// extension MyString on String {
-//   String capitalize() =>
-//       this[0].toUpperCase() + this.substring(1).toLowerCase();
-// }
